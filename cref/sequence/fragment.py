7f435898194905ebd86b00f6f412f625629c87d1
Michele Silva <michele.silva@gmail.com>
2015-01-28 12:27:01 -0200

    Refactoring

--- cref/sequence/fragment.py
+++ cref/sequence/fragment.py
@@ -5,9 +5,9 @@ def fragment(sequence, size=5):
 
     :param sequence: String containing the sequence
     :param size: Size of the window
+
+    :return: a fragment of the sequence with the given size
     """
     for i in range(len(sequence) - size + 1):
         yield sequence[i: i + size]
-
-
 
