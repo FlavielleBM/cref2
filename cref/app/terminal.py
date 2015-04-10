c2ed0829f1313ffe04b242be0c644492b5b2311c
Michele Silva <michele.s@icanlocalize.com>
2015-04-09 23:53:38 -0300

    Refactoring + enable whole process to run

--- cref/app/terminal.py
+++ cref/app/terminal.py
@@ -10,8 +10,9 @@ from cref import sequence
 from cref.sequence.alignment import Blast
 from cref.structure import PDB, torsions, plot
 from cref.structure import write_pdb
-# from cref.structure.clustering import cluster_torsion_angles
-from cref.structure.secondary import SecondaryStructureBD
+from cref.structure.clustering import cluster_torsion_angles
+from cref.structure.secondary import (SecondaryStructureDB,
+                                      predict_secondary_structure)
 
 
 class TerminalApp:
@@ -21,7 +22,7 @@ class TerminalApp:
         self.blast = Blast(db='tests/blastdb/pdbseqres')
         self.fragment_size = 7
         self.central = floor(self.fragment_size / 2)
-        self.ss_bd = SecondaryStructureBD()
+        self.ss_db = SecondaryStructureDB()
 
     def get_central_angles(self, angles, hsp):
         central = self.central
@@ -34,11 +35,11 @@ class TerminalApp:
                 angles['residues'][pos], angles['phi'][pos], angles['psi'][pos])
         return ('', None, None)
 
-    def get_hsp_structure(self, pdb, chain, fragment, hsp, angles):
+    def get_hsp_structure(self, pdb, chain, fragment, ss, hsp, angles):
         residue, phi, psi = self.get_central_angles(angles, hsp)
-        ss_bd_result = self.ss_bd.retrieve(pdb, chain)
-        if phi and psi and ss_bd_result:
-            hsp_seq, hsp_ss = ss_bd_result
+        ss_db_result = self.ss_db.retrieve(pdb, chain)
+        if phi and psi and ss_db_result:
+            hsp_seq, hsp_ss = ss_db_result
             start = hsp.sbjct_start - hsp.query_start
             end = hsp.sbjct_end + self.fragment_size - (
                 hsp.query_end)
@@ -49,17 +50,19 @@ class TerminalApp:
                 pdb=pdb,
                 chain=chain,
                 fragment=fragment,
+                fragment_ss=ss,
                 subject=hsp.sbjct,
-                sequence=hsp_seq[start:end],
-                structure=hsp_ss[start:end],
+                subject_full=hsp_seq[start:end],
+                subject_ss=hsp_ss[start:end],
                 identity=identity,
                 score=hsp.score,
                 phi=round(phi, 2),
                 psi=round(psi, 2),
             )
 
-    def get_structures_for_blast(self, fragment, blast_results):
+    def get_structures_for_blast(self, fragment, ss, blast_results):
         blast_structures = []
+
         for blast_result in blast_results:
             try:
                 pdb_code = blast_result.pdb_code
@@ -73,40 +76,46 @@ class TerminalApp:
                 )
                 for hsp in blast_result.hsps:
                     structure = self.get_hsp_structure(
-                        pdb_code, chain, fragment, hsp, angles)
+                        pdb_code, chain, fragment, ss, hsp, angles)
                     if structure:
                         blast_structures.append(structure)
 
             except Exception as error:
-                import pdb; pdb.set_trace()
                 logging.error("Could not download " + pdb_code)
                 logging.error(error)
         return blast_structures
 
     def run(self, aa_sequence):
-        cref_results = []
+        # Aminoacids in the beggining have unknown phi and psi
+        dihedral_angles = [(None, None)] * (self.central - 1)
 
         for fragment in sequence.fragment(aa_sequence, self.fragment_size):
+            prediction = predict_secondary_structure(fragment)
+            ss = prediction.secondary_structure if prediction else None
+
             blast_results = self.blast.align(fragment)
             blast_structures = self.get_structures_for_blast(
-                fragment, blast_results)
+                fragment, ss, blast_results)
 
             blast_structures = pandas.DataFrame(
                 blast_structures,
-                columns=['pdb', 'chain', 'fragment', 'subject', 'sequence',
-                         'structure', 'identity', 'score', 'phi', 'psi']
+                columns=[
+                    'pdb', 'chain', 'fragment', 'subject', 'subject_full',
+                    'fragment_ss', 'subject_ss', 'identity', 'score',
+                    'phi', 'psi'
+                ]
             )
             blast_structures = blast_structures.sort(
                 ['identity', 'score'], ascending=[0,  0])
             print(blast_structures.to_string(index=False))
-
             plot.ramachandran(blast_structures, fragment, self.central)
-            # secondary_structure = predict_secondary_structure(fragment)
-            # clusters = cluster_torsion_angles(torsion_angles)
-            # central_angles = clusters[secondary_structure[self.central]]
-            # fragment_angles.append(central_angles)
+            clusters = cluster_torsion_angles(blast_structures)
+            central_angles = clusters[ss[self.central]]
+            dihedral_angles.append(central_angles)
 
-        write_pdb(aa_sequence, cref_results, self.central, 'test.pdb')
+        # Amino acids in the end have unbound angles
+        dihedral_angles += [(None, None)] * (self.central)
+        write_pdb(aa_sequence, dihedral_angles, self.central, 'test.pdb')
 
 
 if __name__ == '__main__':
